1b387ed2b891d6282776e7421afaab2a5dde34cf
Paul Penketh <paul.penketh@gmail.com>
2022-05-17 12:45:56 +0100

    Initial Commit

---
+++ LottieTesting/LottieTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieTestingApp.swift
+//  LottieTesting
+//
+//  Created by PAUL PENKETH on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LottieTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
